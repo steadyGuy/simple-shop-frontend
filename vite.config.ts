576dd668710636d188b455e324fc5c3e73328ad4
Yurii Soproniuk <sopronukCC@gmail.com>
2024-03-11 16:36:00 +0200

    github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig((configEnv) => {
   const isDevelopment = configEnv.mode === "development";
 
   return {
+    base: "/<REPO>/",
     plugins: [react()],
     server: {
       watch: {
