4aa759767711bb2f0b9d79be7d6571e3c34db740
Yurii Soproniuk <sopronukCC@gmail.com>
2024-03-11 16:39:19 +0200

    Updates

---
+++ assets/NotFoundPage-939e8029.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ca0c0126.js";const o=()=>t.jsx("div",{style:{textAlign:"center"},children:t.jsx("h1",{className:"logo-font",children:"Not Found Page"})});export{o as default};
